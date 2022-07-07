2b91e6c56d3e029794829ea18ddb16e9b9b2f520
astanil <astakhovnil@gmail.com>
2022-07-07 20:27:57 +0300

    Added port 5000 in sg

--- security_groups.tf
+++ security_groups.tf
@@ -29,6 +29,15 @@ resource "aws_security_group_rule" "allow_http" {
   security_group_id = aws_security_group.web_server_sg.id
 }
 
+resource "aws_security_group_rule" "allow_app" {
+  type              = "ingress"
+  from_port         = 5000
+  to_port           = 5000
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.web_server_sg.id
+}
+
 resource "aws_security_group_rule" "allow_all_out" {
   type              = "egress"
   from_port         = 0
